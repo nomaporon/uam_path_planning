707c1ea5d269d882ad119ac496bced669a91419f
Masaharu Ono <ono.9yuta.2masa.5@gmail.com>
2024-10-21 00:31:52 +0900

    add test.py

--- geo_simulation_project/path_generation/test.py
+++ geo_simulation_project/path_generation/test.py
@@ -4,16 +4,21 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 # Problem parameters
-N = 50  # Number of waypoints
+N = 40  # Number of waypoints
 dim = 2  # 2D problem
 obstacle_center_point = np.array([31.034679, -9.07367])
 r_max = 1.1  # Maximum ratio between consecutive segments
 theta_max = np.pi / 6  # Maximum angle between consecutive segments
 obstacle_radius = 2.0  # Radius of the obstacle
 w_dist, w_obs = 1.0, 500.0  # Weights for distance cost, obstacle penalty, and deviation from initial path
+z0 = np.array([35.590685, -27.711422])  # Start point
+# z0 = np.array([-25, 35])  # Start point
+zN = np.array([26.478673, 9.564082])  # End point
+# zN = np.array([35, -25])  # Start point
 
 # Define the optimization variables
 z = cs.SX.sym('z', N * dim)
+# print(z)
 z_end_points = cs.SX.sym('z_end_points', 4)
 
 z_start = z_end_points[:2]
@@ -59,6 +64,33 @@ def constraint_function(z):
 
     return cs.vertcat(*constraints)
 
+def create_x_init(displacement=0):
+    a = np.linalg.norm(zN - z0) / 2
+    if abs(displacement) > 1:
+        raise ValueError(f'abs(displacement) = {abs(displacement)} must be smaller than 1')
+    out = np.zeros(2*N)
+    if displacement == 0:
+        t = np.linspace(z0[0], zN[0], N+2)[1:-1]
+        out[0::2] = t
+        t = np.linspace(z0[1], zN[1], N+2)[1:-1]
+        out[1::2] = t
+        return out
+    # Create circle arc from x0 to xf
+    b = displacement * a  # distance of chord from midpoint (x0 + xf) / 2
+    v = z0 - zN
+    alpha = np.arctan2(v[1], v[0])
+    R = np.array([[np.cos(alpha), -np.sin(alpha)], [np.sin(alpha), np.cos(alpha)]])
+    beta = 2 * np.arctan(2*a*b / (a**2-b**2))
+    radius = (a**2+b**2) / (2*b)
+    t = np.linspace((np.pi-beta)/2, (np.pi+beta)/2, N+2)[1:-1]
+    ell = R @ np.vstack((radius*np.cos(t), (b**2-a**2)/(2*b) + radius*np.sin(t)))
+    C = (zN + z0) / 2
+    ell[0, :] += C[0]
+    ell[1, :] += C[1]
+    out[0::2] = ell[0, :]
+    out[1::2] = ell[1, :]
+    return out
+
 # Create an OpenGen problem
 problem = og.builder.Problem(z, z_end_points, get_cost(z)) \
             .with_penalty_constraints(constraint_function(z))
@@ -83,18 +115,16 @@ solver = og.tcp.OptimizerTcpManager('python_build/flying_car_optimizer')
 solver.start()
 
 # Generate initial trajectory
-z0 = np.array([35.590685, -27.711422])  # Start point
-# z0 = np.array([-25, 35])  # Start point
-zN = np.array([26.478673, 9.564082])  # End point
-# zN = np.array([35, -25])  # Start point
 end_points = np.array([z0, zN]).flatten()
 init_trajectory = (np.linspace(z0, zN, N + 2)[1:-1]).flatten()
+# displacements = np.arange(-1, 2) / 4
+# print(displacements)
+# init_trajectory = create_x_init(displacements[2])
 
 solver.ping()
 
 try:
     server_response = solver.call(end_points, initial_guess=init_trajectory)
-    # server_response = solver.call(end_points)
     if server_response.is_ok():
         solution = server_response.get()
         u_star = solution.solution
@@ -107,6 +137,7 @@ try:
         
         # Plot the result
         plt.figure(figsize=(10, 8))
+        # plt.figure()
         plt.plot([z0[0], *init_trajectory[::2], zN[0]], [z0[1], *init_trajectory[1::2], zN[1]], 'g--', label='Initial path')
         plt.plot([z0[0], *u_star[::2], zN[0]], [z0[1], *u_star[1::2], zN[1]], 'bo-', label='Optimized path')
         plt.plot(z0[0], z0[1], 'go', markersize=10, label='Start')
@@ -119,8 +150,9 @@ try:
         plt.title('Optimized Flying Car Path')
         plt.legend()
         plt.grid(True)
-        plt.xlim(-30, 40)
-        plt.ylim(-30, 40)
+        plt.xlim(20, 40)
+        plt.ylim(-30, 15)
+        plt.gca().set_aspect('equal', adjustable='box') 
         plt.show()
     else:
         print("Error message:", server_response.get().message)
